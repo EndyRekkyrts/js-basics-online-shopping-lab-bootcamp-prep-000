fec4da90931b52f8cef90702398c9b35242c25ca
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-19 18:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,6 +33,18 @@ function viewCart() {
       var price = itemAndPrice[item];
       itemsAndPrices.push(`${item} at \$${price}`);
     }
+  
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ");
+      break;
+    default:
+      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+      itemsAndPrices = itemsAndPrices.join(", ");
+  }
+
   }
   console.log(`In your cart, you have ${itemsAndPrices.join(', ')}.`);
 }
